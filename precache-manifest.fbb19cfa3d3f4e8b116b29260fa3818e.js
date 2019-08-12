fa380f12a0c4858779ac79e5d04d8b4736798d5e
Wiktor Sacharczuk <44320437+wiktorsachar@users.noreply.github.com>
2019-08-12 18:45:13 +0200

    Add files via upload

---
+++ precache-manifest.fbb19cfa3d3f4e8b116b29260fa3818e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479e8b2ca44b80319e3ad6f36dc346c9",
+    "url": "/ImprisonmentCalculator/index.html"
+  },
+  {
+    "revision": "005576cfc939ae1a9ea2",
+    "url": "/ImprisonmentCalculator/static/css/main.1f0b50c8.chunk.css"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/ImprisonmentCalculator/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "005576cfc939ae1a9ea2",
+    "url": "/ImprisonmentCalculator/static/js/main.fa699772.chunk.js"
+  },
+  {
+    "revision": "c3f01f1bf0114ca230ea",
+    "url": "/ImprisonmentCalculator/static/js/runtime~main.a3c094da.js"
+  }
+]);
